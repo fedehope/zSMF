ffa84ad09ccf5b1959954ffdb9795682bcb979b5
Federico Speranza <federico.speranza.20@ucl.ac.uk>
2023-09-29 16:20:00 +0100

    removed all fig, ax from Plotter. Using just plt

--- Plotter.py
+++ Plotter.py
@@ -17,18 +17,15 @@ class Plotter(BGS, EmceeRun):
     def plot_selected_data(self, zmin, zmax):
         z, x, x_median, w_spec, vmax = self.select_galaxies(zmin, zmax)
 
-        fig, ax = plt.subplots(figsize=(8, 6))
-        ax.scatter(self.z, self.x_median, s=1, c='lightgrey', label='BGS Bright')
-        ax.scatter(z, x_median, s=1, c='C0', label='BGS Bright Mlim')
-        ax.plot(self.z_lin, self.mass_completeness_limit(self.z_lin), color='orange', linewidth=0.9)
+        plt.scatter(self.z, self.x_median, s=1, c='lightgrey', label='BGS Bright')
+        plt.scatter(z, x_median, s=1, c='C0', label='BGS Bright Mlim')
+        plt.plot(self.z_lin, self.mass_completeness_limit(self.z_lin), color='orange', linewidth=0.9)
 
-        ax.legend(loc='lower right', fontsize=15, markerscale=10, handletextpad=0.1)
-        ax.set_xlabel(r"redshift ($z$)", fontsize=15)
-        ax.set_xlim(0., 0.65)
-        ax.set_ylabel(r"best-fit $\log M_*$", fontsize=15)
-        ax.set_ylim(6., 13.2)
-
-        return fig, ax
+        plt.legend(loc='lower right', fontsize=10, markerscale=10, handletextpad=0.1)
+        plt.xlabel(r"redshift ($z$)")
+        plt.xlim(0., 0.65)
+        plt.ylabel(r"best-fit $\log M_*$")
+        plt.ylim(6., 13.2)
 
     def plot_emcee_samples(self):
 
@@ -67,20 +64,17 @@ class Plotter(BGS, EmceeRun):
 
     @staticmethod
     def plot_vmax_hist(h, b, _h, _b, **plot_params):
-        fig, ax = plt.subplots(figsize=(8, 6))
-        ax.step(b[:-1], h, where='pre', linewidth=0.8, **plot_params)
-        ax.step(_b[:-1], _h, where='pre', color='k', linestyle='--', linewidth=0.8)
-        ax.set_yscale('log')
-        ax.set_ylim(1e-5, 4e-2)
-        ax.set_xlim(7, 13)
+        plt.step(b[:-1], h, where='pre', linewidth=0.8, **plot_params)
+        plt.step(_b[:-1], _h, where='pre', color='k', linestyle='--', linewidth=0.8)
+        plt.yscale('log')
+        plt.ylim(1e-5, 4e-2)
+        plt.xlim(7, 13)
 
-        ax.set_xlabel(r'$\log M_*$', fontsize=15)
-        ax.set_ylabel(r'$p(\log M_*)$ [$({\rm Mpc}/h)^{-3}{\rm dex}^{-1}$]', fontsize=15)
-        return fig, ax
+        plt.xlabel(r'$\log M_*$')
+        plt.ylabel(r'$p(\log M_*)$ [$({\rm Mpc}/h)^{-3}{\rm dex}^{-1}$]')
 
     @staticmethod
     def plot_zschechter(x, z0, norm, best_params, **plot_params):
-        # fig, ax = plt.subplots(figsize=(8, 6))
         a0, a1, a2, a3 = best_params
         zschechter = ZSchechterModel.phi(x, z0, a0, a1, a2, a3)
         plt.plot(x, norm * zschechter, **plot_params)
@@ -89,6 +83,5 @@ class Plotter(BGS, EmceeRun):
         plt.ylim(1e-5, 4e-2)
         plt.xlim(7, 13)
 
-        plt.xlabel(r'$\log M_*$', fontsize=15)
-        plt.ylabel(r'$p(\log M_*)$ [$({\rm Mpc}/h)^{-3}{\rm dex}^{-1}$]', fontsize=15)
-        # return fig, ax
+        plt.xlabel(r'$\log M_*$')
+        plt.ylabel(r'$p(\log M_*)$ [$({\rm Mpc}/h)^{-3}{\rm dex}^{-1}$]')
