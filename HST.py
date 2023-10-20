db35d6f1ea2d76036b5dac738b9415884b6773c2
Federico Speranza <federico.speranza.20@ucl.ac.uk>
2023-10-20 15:01:56 +0100

    added HST.py

---
+++ HST.py
@@ -0,0 +1,56 @@
+import numpy as np
+from astropy.cosmology import FlatLambdaCDM
+from scipy.interpolate import interp1d
+
+from Data import Data
+
+
+class HST(Data):
+    def __init__(self, file):
+        super().__init__(file)
+        self.footprint = 0.25 # deg^2
+        self.sky = 41253 # deg^2
+        self.cosmo = FlatLambdaCDM(H0=67.8, Om0=0.307)
+
+        hst_all = self.get_data()
+        self.z = hst_all['ZPDF'].data
+        self.x = hst_all['MASS_MED'].data
+        self.vmax = None
+
+    def select_galaxies(self, zmin, zmax):
+        """Return z, x, x_median, w_spec, vmax for galaxies between (zmin,zmax)"""
+        mask_zlim = (self.z > zmin) & (self.z < zmax)
+
+        z = self.z[mask_zlim]
+        x = self.x[mask_zlim]
+        mask_mlim = (x > self.mass_completeness_limit(z))
+
+        return z[mask_mlim], x[mask_mlim]
+
+    def mass_completeness_limit(self, z):
+
+        x_values = [0.65, 1.0, 1.5, 2.1, 3.0]
+        y_values = [8.72, 9.07, 9.63, 9.79, 10.15]
+
+        f = interp1d(x_values, y_values)
+        return f(z)
+
+    def zmax(self, mstar):
+        y_values = [8.72, 9.07, 9.63, 9.79, 10.15]
+        x_values = [0.65, 1.0, 1.5, 2.1, 3.0]
+
+        f = interp1d(y_values, x_values)
+        if mstar > y_values[-1]:
+            return x_values[-1]
+        else:
+            return f(mstar)
+
+
+    def set_vmax(self):
+        dmin3 = self.cosmo.comoving_distance(0.65).value ** 3
+        self.vmax = np.array([4 * np.pi / 3 * self.footprint / self.sky * (self.cosmo.comoving_distance(self.zmax(m_i)).value ** 3 - dmin3) for m_i in self.x])
+
+
+    def get_number_galaxies(self):
+        if self.z.shape[0] == self.x.shape[0]:
+            return self.z.shape[0]
