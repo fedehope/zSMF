5547418c9acf2502cefb95c5cbbc034a5ea2035e
Federico Speranza <federico.speranza.20@ucl.ac.uk>
2023-10-20 17:41:19 +0100

    added VmaxDensityHst.py + editing

--- HST.py
+++ HST.py
@@ -8,8 +8,8 @@ from Data import Data
 class HST(Data):
     def __init__(self, file):
         super().__init__(file)
-        self.footprint = 0.25 # deg^2
-        self.sky = 41253 # deg^2
+        self.footprint = 0.25  # deg^2
+        self.sky = 41253  # deg^2
         self.cosmo = FlatLambdaCDM(H0=67.8, Om0=0.307)
 
         hst_all = self.get_data()
@@ -35,7 +35,7 @@ class HST(Data):
         f = interp1d(x_values, y_values)
         return f(z)
 
-    def zmax(self, mstar):
+    def zmax_lim(self, mstar):
         y_values = [8.72, 9.07, 9.63, 9.79, 10.15]
         x_values = [0.65, 1.0, 1.5, 2.1, 3.0]
 
@@ -45,12 +45,14 @@ class HST(Data):
         else:
             return f(mstar)
 
-
     def set_vmax(self):
         dmin3 = self.cosmo.comoving_distance(0.65).value ** 3
-        self.vmax = np.array([4 * np.pi / 3 * self.footprint / self.sky * (self.cosmo.comoving_distance(self.zmax(m_i)).value ** 3 - dmin3) for m_i in self.x])
+        self.vmax = np.array([4 * np.pi / 3 * self.footprint / self.sky * (
+                self.cosmo.comoving_distance(self.zmax_lim(m_i)).value ** 3 - dmin3) for m_i in self.x])
 
-
-    def get_number_galaxies(self):
-        if self.z.shape[0] == self.x.shape[0]:
+    def get_number_galaxies(self, bin=False, zmin=None, zmax=None):
+        if self.z.shape[0] == self.x.shape[0] and bin==False:
             return self.z.shape[0]
+        if bin:
+            z, x = self.select_galaxies(zmin, zmax)
+            return z.shape[0]
---
+++ VMaxDensityHst.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from BGS import BGS
+from astropy.cosmology import Planck13
+
+from HST import HST
+
+
+class VmaxDensityHst(HST):
+    def __int__(self, hst_obj):
+        HST.__init__(hst_obj.file)
+
+    def histogram_norm(self, zmin, zmax, bins=40):
+        z, x = self.select_galaxies(zmin, zmax)
+        self.set_vmax()
+        vmax = self.vmax
+        weights = (1. / vmax)
+        hist, bin_edges = np.histogram(x, bins=bins, range=(6., 13.), weights=weights)
+
+        v_zmax = 4 * np.pi / 3 * self.footprint / self.sky * (self.cosmo.comoving_distance(zmax).value ** 3)
+        v_zmin = 4 * np.pi / 3 * self.footprint / self.sky * (self.cosmo.comoving_distance(zmin).value ** 3)
+
+        mask = (self.z > zmin) & (self.z < zmax)
+        _w = 1. / (self.vmax.clip(v_zmin, v_zmax) - v_zmin)
+        _h, _b = np.histogram(self.x[mask], bins=bins, range=(6., 13.), weights=_w[mask])
+        return hist, bin_edges, _h, _b
