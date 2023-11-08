29c6506f4894188bdd209c56f777e7a9dea0314e
Federico Speranza <federico.speranza.20@ucl.ac.uk>
2023-11-08 21:11:55 +0000

    3 catalogs together

--- COSMOS.py
+++ COSMOS.py
@@ -1,15 +1,21 @@
 from scipy.interpolate import interp1d
-
+from astropy.cosmology import FlatLambdaCDM, Planck13
 from Data import Data
 
 
 class COSMOS(Data):
     def __init__(self, file):
         super().__init__(file)
+        self.footprint = 1.38  # deg^2
+        self.sky = 41253  # deg^2
+        self.cosmo = FlatLambdaCDM(H0=67.8, Om0=0.307)
 
         cosmos_all = self.get_data()
-        self.z = cosmos_all['ZPDF'].data
-        self.x = cosmos_all['MASS_MED'].data
+
+        mask = (cosmos_all['FLAG_HJMCC']==0) & (cosmos_all['FLAG_COSMOS']==1) & (cosmos_all['FLAG_PETER']==0)
+
+        self.z = cosmos_all['ZPDF'][mask].data
+        self.x = cosmos_all['MASS_MED'][mask].data
 
     def select_galaxies(self, zmin, zmax):
         """Return z, x, x_median, w_spec, vmax for galaxies between (zmin,zmax)"""
@@ -27,6 +33,17 @@ class COSMOS(Data):
 
         f = interp1d(x_values, y_values)
         return f(z)
+
+
+    def zmax_lim(self, mstar):
+        y_values = [8.1, 8.7, 9.1, 9.3, 9.7, 9.9, 10.0, 10.1, 10.1, 10.1]
+        x_values = [0.175, 0.5, 0.8, 1.125, 1.525, 2.0, 2.5, 3.125, 3.75, 4.4]
+
+        f = interp1d(y_values, x_values)
+        if mstar > y_values[-1]:
+            return x_values[-1]
+        else:
+            return f(mstar)
 
     def get_number_galaxies(self):
         if self.z.shape[0] == self.x.shape[0]:
---
+++ PlotterCOSMOS.py
@@ -0,0 +1,185 @@
+import corner
+import numpy as np
+
+from COSMOS import COSMOS
+from EmceeRun import EmceeRun
+from HST import HST
+from NoZSchechterModel import NoZSchechterModel
+from ZSchechterModel import ZSchechterModel
+
+# -- plotting --
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+class PlotterCOSMOS(COSMOS, EmceeRun):
+    def __init__(self, cosmos, emcee_run):
+        COSMOS.__init__(self, cosmos.file)
+        self.samples = emcee_run.samples
+        self.labels4 = emcee_run.labels4
+        self.labels2 = emcee_run.labels2
+        self.labels3 = emcee_run.labels3
+        self.labels6 = emcee_run.labels6
+        self.labels8 = emcee_run.labels8
+        self.flat_samples = None
+        self.z_lin = np.linspace(0.175, 4.4, 100)
+
+    def plot_selected_data(self, zmin, zmax):
+        z, x = self.select_galaxies(zmin, zmax)
+
+        plt.scatter(self.z, self.x, s=1, c='lightgrey', label='COSMOS')
+        plt.scatter(z, x, s=1, c='C0', label='COSMOS Mlim')
+        plt.plot(self.z_lin, self.mass_completeness_limit(self.z_lin), color='orange', linewidth=0.9)
+
+        plt.legend(loc='lower right', fontsize=10, markerscale=10, handletextpad=0.1)
+        plt.xlabel(r"redshift ($z$)")
+        plt.xlim(0., 4.5)
+        plt.ylabel(r"$\log M_*$")
+        plt.ylim(6., 14.)
+
+    def plot_emcee_samples(self):
+        ndim = len(self.labels4)
+
+        fig, axes = plt.subplots(ndim, figsize=(10, 10), sharex=True)
+        for i in range(ndim):
+            ax = axes[i]
+            ax.plot(self.samples[:, :, i], "k", alpha=0.3)
+            ax.set_xlim(0, len(self.samples))
+            ax.set_ylabel(self.labels4[i])
+            ax.yaxis.set_label_coords(-0.1, 0.5)
+            axes[-1].set_xlabel("step number")
+
+    def plot_emcee_samples2(self):
+        ndim = len(self.labels2)
+
+        fig, axes = plt.subplots(ndim, figsize=(10, 10), sharex=True)
+        for i in range(ndim):
+            ax = axes[i]
+            ax.plot(self.samples[:, :, i], "k", alpha=0.3)
+            ax.set_xlim(0, len(self.samples))
+            ax.set_ylabel(self.labels4[i])
+            ax.yaxis.set_label_coords(-0.1, 0.5)
+            axes[-1].set_xlabel("step number")
+
+    def plot_emcee_samples3(self):
+        ndim = len(self.labels3)
+
+        fig, axes = plt.subplots(ndim, figsize=(10, 10), sharex=True)
+        for i in range(ndim):
+            ax = axes[i]
+            ax.plot(self.samples[:, :, i], "k", alpha=0.3)
+            ax.set_xlim(0, len(self.samples))
+            ax.set_ylabel(self.labels3[i])
+            ax.yaxis.set_label_coords(-0.1, 0.5)
+            axes[-1].set_xlabel("step number")
+
+    def plot_emcee_samples8(self):
+        ndim = len(self.labels8)
+
+        fig, axes = plt.subplots(ndim, figsize=(10, 10), sharex=True)
+        for i in range(ndim):
+            ax = axes[i]
+            ax.plot(self.samples[:, :, i], "k", alpha=0.3)
+            ax.set_xlim(0, len(self.samples))
+            ax.set_ylabel(self.labels8[i])
+            ax.yaxis.set_label_coords(-0.1, 0.5)
+            axes[-1].set_xlabel("step number")
+
+
+    def plot_emcee_samplesZ(self):
+        ndim = len(self.labels6)
+
+        fig, axes = plt.subplots(ndim, figsize=(10, 10), sharex=True)
+        for i in range(ndim):
+            ax = axes[i]
+            ax.plot(self.samples[:, :, i], "k", alpha=0.3)
+            ax.set_xlim(0, len(self.samples))
+            ax.set_ylabel(self.labels6[i])
+            ax.yaxis.set_label_coords(-0.1, 0.5)
+            axes[-1].set_xlabel("step number")
+
+    @staticmethod
+    def plot_emcee_corner(flat_samples, labels):
+        fig = corner.corner(flat_samples, labels=labels, quantiles=(0.16, 0.50, 0.84), show_titles=True);
+        # plt.show()
+
+    @staticmethod
+    def plot_vmax_hist(h, b, _h, _b, **plot_params):
+        plt.step(b[:-1], h, where='post', linewidth=0.8, **plot_params)
+        # plt.step(_b[:-1], _h, where='post', color='k', linestyle='--', linewidth=0.8)
+        plt.yscale('log')
+        plt.ylim(1e-5, 4e-2)
+        plt.xlim(7, 13)
+
+        plt.xlabel(r'$\log M_*$')
+        plt.ylabel(r'$p(\log M_*)$ [$({\rm Mpc}/h)^{-3}{\rm dex}^{-1}$]')
+
+    @staticmethod
+    def plot_zschechter(x, z0, norm, best_params, **plot_params):
+        a0, a1, a2, a3 = best_params
+        zschechter = ZSchechterModel.phi(x, z0, a0, a1, a2, a3)
+        plt.plot(x, norm * zschechter, **plot_params)
+
+        plt.yscale('log')
+        plt.ylim(1e-5, 4e-2)
+        plt.xlim(7, 13)
+
+        plt.xlabel(r'$\log M_*$', fontsize=15)
+        plt.ylabel(r'$p(\log M_*)$ [$({\rm Mpc}/h)^{-3}{\rm dex}^{-1}$]', fontsize=15)
+
+    @staticmethod
+    def plot_zschechter_double(x, z0, norm, best_params, **plot_params):
+        a0, a1, a2, a3, a4, a5, a6 = best_params
+        zschechter = ZSchechterModel.phi_double_hst(x, z0, a0, a1, a2, a3, a4, a5, a6)
+        plt.plot(x, norm * zschechter, **plot_params)
+
+        plt.yscale('log')
+        # plt.ylim(1e-5, 4e-2)
+        plt.xlim(7, 13)
+
+        plt.xlabel(r'$\log M_*$', fontsize=15)
+        plt.ylabel(r'$p(\log M_*)$ [$({\rm Mpc}/h)^{-3}{\rm dex}^{-1}$]', fontsize=15)
+
+    @staticmethod
+    def plot_NoZschechter(x, best_params, **plot_params):
+        logM, alpha1 = best_params
+        nozschechter = NoZSchechterModel.phi(x, logM, alpha1)
+        plt.plot(x, nozschechter, **plot_params)
+
+        plt.yscale('log')
+        plt.ylim(1e-5, 4e2)
+        plt.xlim(7, 13)
+
+        plt.xlabel(r'$\log M_*$', fontsize=15)
+        plt.ylabel(r'$p(\log M_*)$ [$({\rm Mpc}/h)^{-3}{\rm dex}^{-1}$]', fontsize=15)
+
+    @staticmethod
+    def plot_NoZschechter_double(x, best_params, **plot_params):
+        logM, alpha1, alpha2 = best_params
+        nozschechter = NoZSchechterModel.phi_double(x, logM, alpha1, alpha2)
+        plt.plot(x, nozschechter, **plot_params)
+
+        plt.yscale('log')
+        plt.ylim(1e-5, 4e2)
+        plt.xlim(7, 13)
+
+        plt.xlabel(r'$\log M_*$', fontsize=15)
+        plt.ylabel(r'$p(\log M_*)$ [$({\rm Mpc}/h)^{-3}{\rm dex}^{-1}$]', fontsize=15)
+
+    @staticmethod
+    # TODO to finish it
+    def plot_zschechter_error(z0, flat_samples, **plot_params):
+        x = np.linspace(6, 13, 100)
+        inds = np.random.randint(len(flat_samples), size=100)
+        for ind in inds:
+            a0, a1, a2, a3 = flat_samples[ind]
+            zschechter = ZSchechterModel.phi(x, z0, a0, a1, a2, a3)
+            norm = ZSchechterModel.normalisation(flat_samples[ind])
+            plt.plot(x, norm * zschechter, **plot_params)
+
+            plt.yscale('log')
+            plt.ylim(1e-5, 4e-2)
+            plt.xlim(7, 13)
+
+            plt.xlabel(r'$\log M_*$', fontsize=15)
+            plt.ylabel(r'$p(\log M_*)$ [$({\rm Mpc}/h)^{-3}{\rm dex}^{-1}$]', fontsize=15)
